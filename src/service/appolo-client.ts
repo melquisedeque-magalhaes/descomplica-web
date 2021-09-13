4e49c7852c042af9c8e6e9465b17eb407f5e44b2
Melqui <melqui.sodre15@gmail.com>
2021-09-13 16:17:01 -0300

    update url prod backend

--- src/service/appolo-client.ts
+++ src/service/appolo-client.ts
@@ -1,6 +1,6 @@
 import { InMemoryCache, ApolloClient } from '@apollo/client'
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:3333/graphql',
+    uri: process.env.BACKEND_URL_PROD || 'http://localhost:3333/graphql',
     cache: new InMemoryCache(),
 })
