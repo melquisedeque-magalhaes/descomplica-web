d260f9e5c63b464d809c4a44bf379ba4ec97a33f
Melqui <melqui.sodre15@gmail.com>
2021-09-13 16:21:46 -0300

    bug fix data students

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import { gql } from '@apollo/client'
 import { GetServerSideProps } from 'next'
-import { 
-  Box, 
-  Flex, 
-  Heading, 
-  Table, 
-  Tr, 
-  Checkbox, 
-  Thead, 
-  Th, 
-  Tbody, 
-  Td, 
-  Text,
-  useBreakpointValue
+import {
+    Box,
+    Flex,
+    Heading,
+    Table,
+    Tr,
+    Checkbox,
+    Thead,
+    Th,
+    Tbody,
+    Td,
+    Text,
+    useBreakpointValue
 } from '@chakra-ui/react'
 import { RiPencilLine, RiDeleteBinLine } from 'react-icons/ri'
 
@@ -46,81 +46,81 @@ export default function Home({ students }: StudentsProps) {
 
     return (
         <Flex direction="column" h="100vh" w="100%">
-                <Header />
+            <Header />
 
-                <Flex
-                    w="100%"
-                    my="6"
-                    maxWidth={1480}
-                    mx="auto"
-                    px={["4", "6"]}
-                >
+            <Flex
+                w="100%"
+                my="6"
+                maxWidth={1480}
+                mx="auto"
+                px={["4", "6"]}
+            >
 
-                    <Box flex="1" bg="gray.800" borderRadius={8} p={["6", "8"]} width="100%">
-                        
-                        <Flex mb="8" justify="space-between" align="center">
-                            <Heading size="lg" fontWeight="normal">Usuarios</Heading>
+                <Box flex="1" bg="gray.800" borderRadius={8} p={["6", "8"]} width="100%">
 
-                            <CreateButton url="/users/create" />
-                        </Flex>
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Usuarios</Heading>
 
-                        <Table colorScheme="whiteAlpha">
-                            <Thead>
-                                <Tr>
-                                    <Th px={["2", "4", "6"]} color="green.300" width="8">
-                                        <Checkbox colorScheme="pink" />
-                                    </Th>
+                        <CreateButton url="/users/create" />
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px={["2", "4", "6"]} color="green.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>
+                                    Usuario
+                                </Th>
+
+                                {
+                                    isWideVersion &&
                                     <Th>
-                                        Usuario
+                                        CPF
                                     </Th>
+                                }
 
-                                    {   
-                                        isWideVersion && 
-                                        <Th>
-                                            CPF
-                                        </Th>
+                                {isWideVersion && <Td></Td>}
+                            </Tr>
+                        </Thead>
+                        {students.map(student => (
+                            <Tbody key={student._id}>
+                                <Tr>
+                                    <Td px={["2", "4", "6"]}>
+                                        <Checkbox colorScheme="pink" />
+                                    </Td>
+
+                                    <Td>
+                                        <Box>
+                                            <Text fontWeight="bold">{student.name}</Text>
+                                            <Text fontSize="sm" color="gray.300">{student.email}</Text>
+                                        </Box>
+                                    </Td>
+
+                                    {
+                                        isWideVersion && (
+                                            <Td>
+                                                {student.cpf}
+                                            </Td>
+                                        )
                                     }
-                                    
-                                    { isWideVersion &&  <Td></Td> }
+
                                 </Tr>
-                            </Thead>
-                            {students.listStudent.map(student => (
-                                <Tbody key={student._id}>
-                                    <Tr>
-                                        <Td px={["2", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
+                            </Tbody>
+                        ))}
+                    </Table>
 
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">{student.name}</Text>
-                                                <Text fontSize="sm" color="gray.300">{student.email}</Text>
-                                            </Box>
-                                        </Td>
-
-                                        {
-                                            isWideVersion && (
-                                                <Td>
-                                                    {student.cpf}
-                                                </Td>
-                                            )
-                                        }
-                                        
-                                    </Tr>
-                                </Tbody>
-                            ))}
-                        </Table>
-
-                        <Pagination />
-                    </Box>
-                </Flex>
+                    <Pagination />
+                </Box>
             </Flex>
+        </Flex>
     )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const { data } = await client.query({
-    query: gql`
+    const { data } = await client.query({
+        query: gql`
       query {
         listStudent {
             _id
@@ -130,11 +130,11 @@ export const getServerSideProps: GetServerSideProps = async () => {
         }
       }
     `
-  })
+    })
 
-  return {
-    props: {
-      students: data.listStudent
+    return {
+        props: {
+            students: data.listStudent
+        }
     }
-  }
 }
